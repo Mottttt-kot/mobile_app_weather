1ab982acde59b59f0953f1fecf7847a99d7c3fc1
Mottttt-kot <matvolodkin@gmail.com>
2022-12-31 20:32:21 +0300

    - add time and location - add bottom sheet menu - add down swipe

--- app/src/main/java/com/matvey/weathe/MainActivity.kt
+++ app/src/main/java/com/matvey/weathe/MainActivity.kt
@@ -1,18 +1,49 @@
 package com.matvey.weathe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.matvey.weathe.R.*
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var buttonsheet:Button;
+    lateinit var button_text_city:Button;
+    lateinit var info_about_day:Button
 
+    private val LOCATION_PERMISSION_REQ_CODE = 1000;
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
+    private val dateFormat = SimpleDateFormat("EEE, d  MMM   yyyy   |   HH:mm")
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -21,14 +52,90 @@ class MainActivity : AppCompatActivity() {
         setContentView(layout.activity_main)
 
         buttonsheet = findViewById(R.id.buttonsheets)
+        button_text_city = findViewById(R.id.buttonsheets)
+        info_about_day = findViewById(R.id.infodaybutton)
+
+
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+
+        // Refresh function for the layout
+        swipeRefreshLayout.setOnRefreshListener{
+            swipeRefreshLayout.isRefreshing = false
+            getCurrentLocation()
+            info_about_day.text = dateFormat.format(Date()).toString()
+
+        }
+
         buttonsheet.setOnClickListener{
             val dialog = BottomSheetDialog(this)
             val view = layoutInflater.inflate(R.layout.dialog_layout,null)
             dialog.setContentView(view)
             dialog.show()
+            getCurrentLocation()
+            info_about_day.text = dateFormat.format(Date()).toString()
 
         }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
 
     }
+
+    /// ------------------------- location code------------------------------------------
+    private fun getCurrentLocation() {
+        val gcd = Geocoder(this, Locale.getDefault())
+        val addresses = gcd.getFromLocation(latitude, longitude, 1)
+
+        // checking location permission
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ_CODE);
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                // getting the last known or current location
+                latitude = location.latitude
+                longitude = location.longitude
+                if (addresses.size > 0)
+                {
+                    button_text_city.text = addresses[0].locality.toString()
+                }
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed on getting current location",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQ_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                } else {
+                    // permission denied
+                    Toast.makeText(this, "You need to grant permission to access location",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    //------------------------------------------------------------------------------------
+
+
+
+    ///-------------------------definition of the day--------------------------------------
+
+
+    //-------------------------------------------------------------------------------------
 
 }
